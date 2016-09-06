d0f896953b05322b6eb755ef4613eb3da0edbb98
Diana Zhou <yuz302@ucsd.edu>
2016-09-06 19:11:44 -0400

    regular expression/get product name and batch number

--- Scraper.py
+++ Scraper.py
@@ -1,5 +1,26 @@
-
 import urllib2
-response = urllib2.urlopen('http://10.10.10.123/user/andon.htm')
-html = response.read()
-print html
+import re
+
+
+def getHtml(url):
+    response = urllib2.urlopen(url)
+    html = response.read()
+    return html
+
+
+def getProductName(html):
+    prod_re = r'<p id="prod">(.*?)</p>'
+    prod_match = re.findall(prod_re, html,re.S|re.M)
+    return prod_match
+
+def getProductBatchNumber(html):
+    prod_re = r'<p id="prodbatch">(.*?)</p>'
+    prod_match = re.findall(prod_re, html, re.S | re.M)
+    return prod_match
+
+if __name__ == '__main__':
+    url = 'http://10.10.10.123/user/andon.htm'
+    html = getHtml(url)
+    print getProductName(html)[0]
+    print getProductBatchNumber(html)[0]
+
